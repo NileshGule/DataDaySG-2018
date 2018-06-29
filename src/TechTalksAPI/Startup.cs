bc63fd89a2c97f7d1b7289bf056fcb6a83ba6fd0
NileshGule <vn_nilesh@yahoo.com>
2018-06-29 19:21:53 +0800

    Added connection to SQL server

--- src/TechTalksAPI/Startup.cs
+++ src/TechTalksAPI/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using TechTalksAPI.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace TechTalksAPI
 {
@@ -26,6 +28,11 @@ namespace TechTalksAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<TechTalksDBContext>
+            (
+                options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
